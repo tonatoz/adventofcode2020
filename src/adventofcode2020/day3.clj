1930a44f6b80a90759ab9a5f78ba06164b3e3993
Anton <tonatoz@gmail.com>
2020-12-03 13:23:11 +0300

    simplfy day3

--- src/adventofcode2020/day3.clj
+++ src/adventofcode2020/day3.clj
@@ -14,7 +14,7 @@
                  ".#..#...#.#"])
 (def input (cm/load-lines 3))
 
-(defn solve [in right down]
+(defn solve [in [right down]]
   (->> (take-nth down in)
        (rest)
        (reduce (fn [[i acc] x]
@@ -27,8 +27,8 @@
 
 (defn solve2 [in]
   (->> [[1 1] [3 1] [5 1] [7 1] [1 2]]
-       (map #(apply (partial solve in) %))
+       (map (partial solve in))
        (apply *)))
 
-(println "Day 3 first answer is" (solve input 3 1))
+(println "Day 3 first answer is" (solve input [3 1]))
 (println "Day 3 second answer is" (solve2 input))

1a6f82428eeed2386e5dfc8b60ae31d972498af4
Anton <tonatoz@gmail.com>
2020-12-05 20:31:01 +0300

    day5

---
+++ src/adventofcode2020/day5.clj
@@ -0,0 +1,42 @@
+(ns adventofcode2020.day5
+  (:require [clojure.string :as str]
+            [adventofcode2020.common :as cm]))
+
+(def test-input "FBFBBFFRLR")
+(def input (cm/load-lines 5))
+
+(defn scan
+  [pass low-bound hight-bound]
+  (first
+   (reduce (fn [[low hight] direction]
+             (let [med (int (+ low (/ (- hight low) 2.0)))]
+               (case direction
+                 (\F \L) [low med]
+                 (\B \R) [(inc med) hight])))
+           [low-bound hight-bound]
+           pass)))
+
+(defn get-place
+  [in]
+  (let [row (scan (take 7 in) 0 127)
+        col (scan (drop 7 in) 0 7)]
+    (+ col (* 8 row))))
+
+(defn find-my-seat
+  [ids]
+  (->> ids
+       (sort)
+       (partition 3 1)
+       (filter (fn [[a b c]] (not= (inc a) b (dec c))))
+       (first)
+       (second)
+       (inc)))
+
+(defn solve
+  [in result-fn]
+  (->> input
+       (map get-place)
+       (result-fn)))
+
+(println "Day 5 first answer" (solve input #(apply max %)))
+(println "Day 5 second answer" (solve input find-my-seat))
